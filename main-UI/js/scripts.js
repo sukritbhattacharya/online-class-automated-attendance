50ddc36f6b6e2932d94bb54455766faef9f3e833
ncsrikanth <srikanthchikku1997@gmail.com>
2020-10-04 11:26:37 +0530

    recognition updated

--- main-UI/js/scripts.js
+++ main-UI/js/scripts.js
@@ -85,7 +85,7 @@
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
   //firebase.analytics();
-  console.log(firebase);
+  //console.log(firebase);
   var database = firebase.database();
   var ref_to_attendance = database.ref('attendance')
   var date = []
@@ -112,7 +112,7 @@ form.addEventListener('submit', (event) => {
     }
   var some = ref_to_attendance.push(dict).key;
   console.log(classDurationInput);
-  console.log(nameInputLoweCase);
+  //console.log(nameInputLoweCase);
 
   if (classDurationInput){
     
@@ -121,17 +121,23 @@ form.addEventListener('submit', (event) => {
 
     let seconds = classDurationInput*60;
     let count =parseInt( seconds/15);
+    let noofRecog = count/3;            
     console.log(count);
+    
   
     //Video Starts------------------------------------------
     
     const video = document.getElementById('video')
 
 Promise.all([
-  faceapi.nets.tinyFaceDetector.loadFromUri('../models'),
-  faceapi.nets.faceLandmark68Net.loadFromUri('../models'),
-  faceapi.nets.faceRecognitionNet.loadFromUri('../models'),
-  faceapi.nets.faceExpressionNet.loadFromUri('../models')
+ // faceapi.nets.mtcnn.loadFromUri('../main-UI/models')
+
+  
+  faceapi.nets.tinyFaceDetector.loadFromUri('../main-UI/models'),
+ // console.log(faceapi.nets)
+  //faceapi.nets.faceLandmark68Net.loadFromUri('../main-UI/models'),
+  //faceapi.nets.faceRecognitionNet.loadFromUri('../main-UI/models'),
+  //faceapi.nets.faceExpressionNet.loadFromUri('../main-UI/models')
 ]).then(startVideo)
 
 //if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
@@ -154,7 +160,7 @@ video.addEventListener('playing', () => {
   
   const canvas = faceapi.createCanvasFromMedia(video)
   
-  document.body.append(canvas)
+  //document.body.append(canvas)
   const displaySize = { width: video.width, height: video.height }
   faceapi.matchDimensions(canvas, displaySize)
   
@@ -162,95 +168,52 @@ video.addEventListener('playing', () => {
   var absent =[];
   var pcount=0;
   var acount=0;
+  var frPcount= 0;
+  var frAcount= 0;
+  var frPresent=[];
+  var frAbsent=[];
 
-  //var endcount = 4
-  var x  = 1
+ // var frResponse = false;
+  var apiCount = 1;
+  var apiC = 0;
+  // var frPlength = frPresent.length;
+  // var frAlength = frAbsent.length; 
+ 
+  
+
+
+
+  var x  = 1;
   var id = NaN
   
   id = setInterval(async () => {
     console.log("count",x)
     
    
-    const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions())
+   const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions())    //{inputSize: 160, scoreThreshold: 0.2}
+   //console.log(detections);
+
+  // const mtcnnForwardParams = {
+  //   // limiting the search space to larger faces for webcam detection
+  //   minFaceSize: 200
+  // }
+  
+  // const mtcnnResults = await faceapi.mtcnn(video, new faceapi.MtcnnOptions())      //mtcnnForwardParams
+  // console.log(mtcnnResults);
+  //faceapi.drawDetection('overlay', mtcnnResults.map(res => res.faceDetection), { withScore:false })
+  //faceapi.drawLandmarks('overlay', mtcnnResults.map(res => res.faceLandmarks), { lineWidth: 4, color: 'red' })
+
+
+
+
+  
     
     
     if(detections.length == 1)
     {
       pcount+=1;
       present.push(pcount);
-     let canvas2 = document.getElementById("gameCanvas").getContext('2d')
-     //console.log(canvas2);
-     canvas2.drawImage(video,0,0)
-    document.getElementById("imageshow").setAttribute("src",document.getElementById("gameCanvas").toDataURL('image/jpg'))
-    let dispImg = document.getElementById("imageshow").src
-     //console.log(dispImg);
-     document.getElementById("imageshow").style.display="none";
-     //Usage example:
-var file = dataURLtoFile(dispImg,'hello.png');
-console.log(file);
-
-//API CALL
-var formdata = new FormData();
-formdata.append("user_id", nameInputLoweCase);
-formdata.append("app_id", "kushi");
-formdata.append("image", file, file.name);
-formdata.append("image2", file, file.name);
-formdata.append("n", "8");                   //n=8 =>liveness and recog, n=1=>liveness, n=100=>recog. 
-formdata.append("deviceOs", "A");
-
-var requestOptions = {
-  method: 'POST',
-  body: formdata,
-  redirect: 'follow'
-};
-
-fetch("https://staging-preprod.vishwamcorp.com/v2/single_gesture_ios", requestOptions)
-  .then(function(response) { 
-  //  console.log("inside the fetch then of api request");
- 
-  let status = response.status;
-  //  console.log(status);
-  let responsetext = {                  //how to do it in array form
-    "200":"success",
-    "404":"Face Recognition Failed!",
-    "500":"Server error!",
-    "424":"Failed Dependency",
-    "409":"Conflict Error.",
-  }
-
-  let statusText = response.statusText;
-  //  console.log();
-   ref_to_attendance.child(some).child(nameInputLoweCase).push(
-    {
-        "timestamp":new Date().toString(),
-        "response":responsetext[status.toString()]
     }
-    )
-    if(status == 200) {
-      document.getElementById("status").innerHTML = status.toString() +" "+ statusText +" "+ "Success!";
-      console.log(statusText);
-    }
-    if(status == 404 || status == 424 || status == 500 || status == 409 || status == 406 ){
-      document.getElementById("status").innerHTML = status.toString() +" "+ statusText;
-      document.getElementById("status").style.color = "red";
-      
-      console.log(statusText);
-    }
-  /*if(status == 404){
-    console.log("face recognition failed")
-  }
-  if(status == 424){
-    console.log()
-  }*/
-  })
-  
-  //.then(result => console.log(result))
-  .catch(function(error){ 
-    console.log('error', error)
-  });
-
-
-}
 
 ////////////////////////
 
@@ -266,7 +229,7 @@ fetch("https://staging-preprod.vishwamcorp.com/v2/single_gesture_ios", requestOp
 	  absent.push(acount);
 	  absentLength = absent.length;
       let canvas2 = document.getElementById("gameCanvas").getContext('2d')
-      console.log(canvas2);
+      //console.log(canvas2);
       canvas2.drawImage(video,0,0)
      //document.getElementById("imageshow").setAttribute("src",document.getElementById("gameCanvas").toDataURL('image/jpg'))
       let c=document.getElementById("gameCanvas")
@@ -274,8 +237,8 @@ fetch("https://staging-preprod.vishwamcorp.com/v2/single_gesture_ios", requestOp
       var image = new Image();
   image.id = "pic";
 image.src = c.toDataURL();
-console.log(image.src)
-var fileType =dataURLtoFile(image.src, 'absent.png');
+//console.log(image.src)
+var fileType =dataURLtoFile(image.src, nameInputLoweCase + 'absent.png');
 var storage = firebase.storage();
 var storageRef = storage.ref('DetectionFailed');
 var imagesRef = storageRef.child(nameINput + " " + "Absent:"+ " "+ absentLength + " " + new Date() ).put(fileType);
@@ -286,79 +249,179 @@ var imagesRef = storageRef.child(nameINput + " " + "Absent:"+ " "+ absentLength 
     
     
     
-     const resizedDetections = faceapi.resizeResults(detections, displaySize)
-     canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
+    // const resizedDetections = faceapi.resizeResults(detections, displaySize)
+    // canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
     // faceapi.draw.drawDetections(canvas, resizedDetections)
     // faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)
-    // faceapi.draw.drawFaceExpressions(canvas, resizedDetections)
+    //faceapi.draw.drawFaceExpressions(canvas, resizedDetections)
     
     
     
-    console.log(present);
+    console.log("present array" + " "+ present);
     console.log(present.length);
-    console.log(absent);
+    console.log("absent array" + " " + absent);
     console.log(absent.length);
-
-    if( x == count)
-    {
-      clearInterval(id)
-      let pl= present.length;
-      //console.log(pl);
-      let al= absent.length;
-      //console.log(al);
-      let total = pl+al;
-      //console.log(total);
-      let ratio = pl/total;
-      console.log(ratio);
-      let pPercentage = ratio*100;
-      let presentPercentage = "The present percentage is" + "" + pPercentage;
-      console.log(presentPercentage);
-      document.getElementById("displayPercentage").innerHTML = presentPercentage;
-      
-  var ref = database.ref('percentages');
-  ref.on('value', gotData, errData);
-
-  function gotData(data){
-  // console.log(data);
-  var percentages = data.val();
-  var keys = Object.keys(percentages);
-  console.log(keys);
-  for(var i = 0; i < keys.length; i++){
-    var k =keys[i];
-    var name = percentages[k].name;
-    var percentage = percentages[k].percentage;
-    let nameByPercent = name + ":" + percentage;
-    let list = document.getElementById("percentage-list");
-    list.appendChild(createMenuItem(nameByPercent))
     
-    //console.log(name, percentage);
-  }
-
-  }
-
-  function errData(err){
-    console.log('Error!')
-    console.log(err);
     
-  }
+      //console.log(x + "count is");
+      if(x%noofRecog == 0){
+        
+      let canvas2 = document.getElementById("gameCanvas").getContext('2d')
+     //console.log(canvas2);
+     canvas2.drawImage(video,0,0)
+    document.getElementById("imageshow").setAttribute("src",document.getElementById("gameCanvas").toDataURL('image/jpg'))
+    let dispImg = document.getElementById("imageshow").src
+     //console.log(dispImg);
+     document.getElementById("imageshow").style.display="none";
+     //Usage example:
+let file = dataURLtoFile(dispImg,'hello.png');
 
-  var data = {
-    name: nameInputLoweCase,
-    percentage: pPercentage
-  }
-  console.log(data);
-  ref.push(data);
- 
-  
-/////////////////////////////////////////////FIRE BASE END//////////////////////////////
+        var formdata = new FormData();
+        formdata.append("user_id", nameInputLoweCase);
+        formdata.append("app_id", "kushi");
+        formdata.append("image", file, file.name);
+        formdata.append("image2", file, file.name);
+        formdata.append("n", "8");                   //n=8 =>liveness and recog, n=1=>liveness, n=100=>recog. 
+        formdata.append("deviceOs", "A");
+
+let requestOptions = {
+  method: 'POST',
+  body: formdata,
+  redirect: 'follow'
+};
+      ///////////////////////////////.then method//////////////////
+//console.log(frResponse);
+fetch("https://staging-preprod.vishwamcorp.com/v2/single_gesture_ios", requestOptions)
+  .then((response) =>{
+    let status = response.status
+    let responsetext = {                  //how to do it in array form
+      "200":"success",
+      "404":"Face Recognition Failed!",
+      "500":"Server error!",
+      "424":"Failed Dependency",
+      "409":"Conflict Error.",
+      "406":"Not Acceptable",
     }
-    
-    x += 1
+  
+    let statusText = response.statusText;
+    //  console.log();
+     ref_to_attendance.child(some).child(nameInputLoweCase).push(
+      {
+          "timestamp":new Date().toString(),
+          "response":responsetext[status.toString()]
+      }
+      )
+      if(status == 200) {
+        document.getElementById("status").innerHTML = status.toString() +" "+ statusText +" "+ "Success!";
+        console.log(statusText);
+        frPcount+=1;
+        frPresent.push(frPcount);
+      }
+      if(status == 404 || status == 424 || status == 500 || status == 409 || status == 406 ){
+         
+        document.getElementById("status").innerHTML = status.toString() +" "+ statusText;
+        document.getElementById("status").style.color = "red";
+
+        frAcount+=1;
+        frAbsent.push(frAcount);
+        
+        console.log(statusText);
+        // console.log(x+"here");
+        // console.log(count+"count is here");
+      }
+
+      if( x == count){
+        // frResponse = true;
+        // console.log(frResponse)
+              // console.log("hiiiiiiiiiiiiiiiiii")
+              clearInterval(id)
+              let pl= present.length;
+              //console.log(pl);
+              let al= absent.length;
+              //console.log(al);
+              let total = pl+al;
+              //console.log(total);
+              let ratio = pl/total;
+              //console.log(ratio);
+              let pPercentage = Math.round(ratio*100);
+              let presentPercentage = nameInputLoweCase+ "s" + "present percentage is" + "" + pPercentage;
+              console.log(presentPercentage);
+              document.getElementById("displayPercentage").innerHTML = presentPercentage;
+              
+          var ref = database.ref('percentages');
+          var data = {
+            name: nameInputLoweCase,
+            percentage: pPercentage
+          }
+          //console.log(data);
+          ref.push(data);
+        
+          ref.on('value', gotData, errData);
+        
+          function gotData(data){
+          // console.log(data);
+          var percentages = data.val();
+          //console.log(percentages);
+          var keys = Object.keys(percentages);
+          //console.log(keys);
+          for(var i = 0; i < keys.length; i++){
+            var k =keys[i];
+            var name = percentages[k].name;
+            var percentage = percentages[k].percentage;
+            let nameByPercent = name + ":" + percentage;
+            let list = document.getElementById("percentage-list");
+            list.appendChild(createMenuItem(nameByPercent))
+            
+            //console.log(name, percentage);
+          }
+        
+          }
+        
+          function errData(err){
+            console.log('Error!')
+            console.log(err);
+            
+          }
+
+          console.log(frAbsent.length)
+          let recogThreshold = frPresent.length + frAbsent.length;
+if(frPresent.length < Math.round( recogThreshold/2)){
+  console.log(frAbsent.length)
+  
+  let frPercentage = pPercentage - frAbsent.length*3;
+  
+  console.log(frPercentage);
+  let frRecog = document.getElementById("fr-percentage")
+  frRecog.innerHTML = frPercentage;
+}
+        }
+
+  })
+  
+  .catch(err => {
+    console.log(err)
+  });
+}
+
+// console.log(x+"end condition" + count + "end");
+
+
 
     
+    
 
+    
+   
+
+if(x<count){
+x +=1;
+}
       
   },15000)
+  
+  
+ 
+  
 
 })
 
@@ -382,8 +445,11 @@ function dataURLtoFile(dataurl, filename) {
   return new File([u8arr], filename, {type:mime});
 }
 
-function createMenuItem(getList) {
+function createMenuItem(getList) {    
   let li = document.createElement('li');
   li.textContent = getList;
   return li;
 }
+
+
+
